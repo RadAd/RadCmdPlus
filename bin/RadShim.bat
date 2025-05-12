72f6501b420e34cb16102b74c78aed34268b13d2
RadAd <adamgates84@gmail.com>
2025-05-12 10:13:08 +1000

    Remove whitespace at end of line in output

--- bin/RadShim.bat
+++ bin/RadShim.bat
@@ -7,14 +7,14 @@ if not defined command (goto :usage)
 if "%command%" == "create" shift & goto :create
 if "%command%" == "check" shift & goto :check
 if "%command%" == "/?" goto :usage
-echo Unknown command: %1 >&2
+echo Unknown command: %1>&2
 goto :eof
 
 :create
-if "%~1" == "" (echo Missing target >&2 & exit /b 1)
-if not "%~2" == "" (echo Too many parameters >&2 & exit /b 1)
+if "%~1" == "" (echo Missing target>&2 & exit /b 1)
+if not "%~2" == "" (echo Too many parameters>&2 & exit /b 1)
 
-if not exist %1 (call RadColorEcho Cannot find target: {error}%1{reset} >&2 & exit /b 1)
+if not exist %1 (call RadColorEcho Cannot find target: {error}%1{reset}>&2 & exit /b 1)
 if not exist %RAD_SHIM_DIR% md %RAD_SHIM_DIR%
 call RadColorEcho Shim: {info}%1{reset}
 echo.@rem Prog=%1> "%RAD_SHIM_DIR%\%~n1.bat"
@@ -28,7 +28,7 @@ goto :eof
 :check
 set _=
 for /f "tokens=1,* delims==" %%i in (%RAD_SHIM_DIR%\%~1.bat) do @(if "%%i"=="@rem Prog" set _=%%j)
-if not defined _ (call RadColorEcho Cannot determine shim program: {warning}%1{reset} >&2 & exit /b 1)
+if not defined _ (call RadColorEcho Cannot determine shim program: {warning}%1{reset}>&2 & exit /b 1)
 if not exist %_% (call RadColorEcho Shim doesn't exist: {error}%_%{reset} & exit /b 1)
 call RadColorEcho Shim exists: {green}%_%{reset}
 goto :eof
